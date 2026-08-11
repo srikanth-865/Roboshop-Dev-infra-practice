10a974ed3c86ee05d6aa63e1053fcfe6834a0958
unknown <srikanthram87@gmail.com>
2026-08-11 13:35:59 +0530

    oil&masala

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -34,7 +34,7 @@ resource "terraform_data" "mongodb" {
   }
   provisioner "remote-exec" {
     command = [
-       "chmod +x /tmp/script.sh"
+       "chmod +x /tmp/script.sh",
        "sudo /tmp/script.sh mongodb ${var.environment} roboshop.yaml"
     
     ]
