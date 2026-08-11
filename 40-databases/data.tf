c3a8b7ce0ae5801c9d46e15c6a477b1c182992b1
unknown <srikanthram87@gmail.com>
2026-08-11 12:47:02 +0530

    oil&masala

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -1,5 +1,5 @@
 data "aws_ssm_parameter" "database_subnet_ids" {
-  name = "/${var.project}/${var.environment}/"database_subnet_ids"
+  name = "/${var.project}/${var.environment}/database_subnet_ids"
 }
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
