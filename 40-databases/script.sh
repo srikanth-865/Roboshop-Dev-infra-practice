6cc24cabef99b98277b64d6f0275b93c7102d1e8
unknown <srikanthram87@gmail.com>
2026-08-11 14:18:44 +0530

    oil&masala

--- 40-databases/script.sh
+++ 40-databases/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 component=$1
 environment=$2
-dnf install ansible -y
+sudo dnf install ansible -y
 mkdir -p /var/log/roboshop1
 chown ec2-user:ec2-user /var/log/roboshop1
 chmod -R 755  /var/log/roboshop1
@@ -10,5 +10,5 @@ touch /var/log/roboshop1/ansible.log
 cd /home/ec2-user
 git clone https://github.com/srikanth-865/roboshop-ansible-v3.git
 cd roboshop-ansible-v3
-git pul
+git pull
 ansible-playbook -e component=$component -e env=$environment roboshop.yaml
