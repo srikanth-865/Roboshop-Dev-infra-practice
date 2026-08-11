ed0e79c4794a8dd834e21c1c7ec6fcb081446bdc
unknown <srikanthram87@gmail.com>
2026-08-11 12:52:28 +0530

    oil&masala

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "mongodb" {
   ami           = local.ami_id
   instance_type = "t3.micro"
   subnet_id     = local.database_subnet_id
-  vpc_security_group_ids      = local.mongodb_sg_id
+  vpc_security_group_ids      = [local.mongodb_sg_id]
   associate_public_ip_address = false
 
   tags = merge(
