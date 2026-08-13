29e72b00a199ba0f4191f4a0f7fc334505828d4b
unknown <srikanthram87@gmail.com>
2026-08-13 18:41:52 +0530

    oil&masala

--- 50-backend_alb/main.tf
+++ 50-backend_alb/main.tf
@@ -2,8 +2,8 @@ resource "aws_lb" "backend_alb" {
   name               = "${local.common_name}-backend-alb" # roboshop-dev-backend-alb
   internal           = true
   load_balancer_type = "application"
-  security_groups    = [local.backend_alb_sg_id]
-  subnets            = local.private_subnet_ids
+  security_groups    = [local.backend-alb_sg_id]
+  subnets            = local.private_subnet_id
 
   enable_deletion_protection = false
 
