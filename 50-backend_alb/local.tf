a1c66371b67ee30a59fd5d16c2ee56421e8a25e3
unknown <srikanthram87@gmail.com>
2026-08-13 18:40:18 +0530

    oil&masala

--- 50-backend_alb/local.tf
+++ 50-backend_alb/local.tf
@@ -1,6 +1,6 @@
  locals{
-    backend-alb_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
-    private_subnet_id = split(",",data.aws_ssm_parameter.database_subnet_ids.value)[0] 
+    backend-alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+    private_subnet_id = split(",",data.aws_ssm_parameter.private_subnet_ids.value)[0] 
     common_tags = {
         project = var.project
         environment = var.environment
