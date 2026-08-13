45e61ddaaa7c4bc19709e801476721c67b192132
unknown <srikanthram87@gmail.com>
2026-08-13 18:26:03 +0530

    Remove backend alb

--- 50-backend_alb/data.tf
+++
@@ -1,7 +0,0 @@
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project}/${var.environment}/private_subnet_ids"
-}
-
-data "aws_ssm_parameter" "backend-alb_sg_id" {
-  name = "/${var.project}/${var.environment}/backend-alb_sg_id"
-}
