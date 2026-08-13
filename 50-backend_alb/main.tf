c06592751ffa80252677148f3eee04b937a9da1d
unknown <srikanthram87@gmail.com>
2026-08-13 18:45:24 +0530

    oil&masala

--- 50-backend_alb/main.tf
+++ 50-backend_alb/main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "backend_alb" {
   internal           = true
   load_balancer_type = "application"
   security_groups    = [local.backend-alb_sg_id]
-  subnets            = local.private_subnet_ids
+  subnets            = local.private_subnet_id
 
   enable_deletion_protection = false
 
