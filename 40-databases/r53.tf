078bf3fc9bdfc40a7f11562431ce745c71e5b80a
unknown <srikanthram87@gmail.com>
2026-08-19 08:32:01 +0530

    oil&masala

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -1,14 +1,15 @@
 resource "aws_route53_record" "mongodb" {
   zone_id = var.zone_id
-  name    = "mongodb-{var.environment}.{var.domain_name}"  
+  name    = "mongodb-${var.environment}.${var.domain_name}"  
   type    = "A"
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
+  
 }
 
 resource "aws_route53_record" "redis" {
   zone_id = var.zone_id
-  name    = "redis-{var.environment}.{var.domain_name}"  
+  name    = "redis-${var.environment}.${var.domain_name}"  
   type    = "A"
   ttl     = 1
   records = [aws_instance.redis.private_ip]
@@ -16,7 +17,7 @@ resource "aws_route53_record" "redis" {
 
 resource "aws_route53_record" "rabbitmq" {
   zone_id = var.zone_id
-  name    = "rabbitmq-{var.environment}.{var.domain_name}"  
+  name    = "rabbitmq-${var.environment}.${var.domain_name}"  
   type    = "A"
   ttl     = 1
   records = [aws_instance.rabbitmq.private_ip]
@@ -24,7 +25,7 @@ resource "aws_route53_record" "rabbitmq" {
 
 resource "aws_route53_record" "mysql" {
   zone_id = var.zone_id
-  name    = "mysql-{var.environment}.{var.domain_name}"  
+  name    = "mysql-${var.environment}.${var.domain_name}"  
   type    = "A"
   ttl     = 1
   records = [aws_instance.mysql.private_ip]
