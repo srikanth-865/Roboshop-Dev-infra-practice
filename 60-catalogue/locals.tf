9cda9c0526ba0e300497cb282ec743d8f74c6d3f
unknown <srikanthram87@gmail.com>
2026-08-19 10:23:06 +0530

    oil&masala

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,5 +1,5 @@
  locals{
-    catalogue_sg_id = data.aws_ssm_parameter.catalogue.value
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     private_subnet_id = split(",",data.aws_ssm_parameter.private_subnet_ids.value)
     common_tags = {
         project = var.project
