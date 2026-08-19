948e015c0d19eba7a1f778d5821b504a89d47f1f
unknown <srikanthram87@gmail.com>
2026-08-19 10:32:04 +0530

    oil&masala

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "catalogue" {
   instance_type = "t3.micro"
-  subnet_id     = local.private_subnet_id
+  subnet_id     = local.private_subnet_id[0]
   vpc_security_group_ids = [local.catalogue_sg_id]
 
 
