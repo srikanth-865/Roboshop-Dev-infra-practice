c47ab9215cad6be2a33aea6c04bdb7f3c090e35e
unknown <srikanthram87@gmail.com>
2026-08-19 11:10:11 +0530

    oil&masala

--- 60-catalogue/script.sh
+++ 60-catalogue/script.sh
@@ -11,7 +11,7 @@ cd home/ec2-user
 git clone https://github.com/srikanth-865/roboshop-ansible-v3.git
 cd roboshop-ansible-v3
 git pull
-ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
 
 
 
