594ca3d6818a42fbfa9bdb23d24172ae2b8c5139
unknown <srikanthram87@gmail.com>
2026-08-19 09:08:07 +0530

    oil&masala

---
+++ 50-backend_alb/parameter.tf
@@ -0,0 +1,6 @@
+resource "aws_ssm_parameter" "aws_alb_listner" {
+  count = length(var.sg_name)
+  name  = "/${var.project}/${var.environment}/backend_alb_listener_arn""  #Roboshop1/Dev/mysql
+  type  = "String"
+  value = aws_lb_listener.http.arn
+}
