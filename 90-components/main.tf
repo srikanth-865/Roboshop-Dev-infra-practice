f143899ce538de727a7656af3dc5650314e08c38
unknown <srikanthram87@gmail.com>
2026-08-28 10:25:31 +0530

    oil&karam

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,7 +1,7 @@
 module "component"{
     for_each = var.components
     source = "git::https://github.com/srikanth-865/terraform-roboshop-component.git?ref=main"
-    component = each.key
+    components = each.key
     environment = var.environment
     app_version = each.value.app_version 
 }
